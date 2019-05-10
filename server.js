15bfa9ed2928822b22030a8d0576d7813e7bc2fb
Edward Apostol <ed@apostol.com>
2019-05-09 23:48:06 -0400

    minor change to repository to test on heroku

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 app.get('/', (req, res) => {
   // eslint-disable-next-line no-console
   console.log('req = \n', req);
-  res.send('Test CI with Travis');
+  res.send('Test CI with Travis 1.0');
 });
 
 const server = app.listen(PORT, () => {
